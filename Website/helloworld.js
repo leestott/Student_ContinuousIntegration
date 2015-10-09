74ce35b3e813337dd399f158be4dd172cedfbac2
thebeebs <martinbeeby@hotmail.com>
2015-10-09 12:00:45 +0100

    Final Push

--- Website/helloworld.js
+++ Website/helloworld.js
@@ -2,6 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('<h1>Imagine there is a boy wearing a T shirt with a Microsoft Logo on\n</h1>');
+  res.end('Imagine there is a boy wearing a T shirt with a Microsoft Logo on\n');
 }).listen(port);
 
