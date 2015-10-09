79f435b5c50bf2eb857fc7e61b4d685d7c538933
thebeebs <martinbeeby@hotmail.com>
2015-10-09 11:49:55 +0100

    Final Push

--- Website/helloworld.js
+++ Website/helloworld.js
@@ -2,6 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello you wonderful people\n');
 }).listen(port);
 
