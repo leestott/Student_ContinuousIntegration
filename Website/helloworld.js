2fa1e3dba7d20563ba1fc95792b8f173ae870ddf
thebeebs <martinbeeby@hotmail.com>
2015-10-09 10:43:56 +0100

    Changing default site

--- Website/helloworld.js
+++ Website/helloworld.js
@@ -4,3 +4,8 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
+
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(port);
