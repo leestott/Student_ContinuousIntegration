9ee788ad68de2afb4cd6715411771b26385df28e
thebeebs <martinbeeby@hotmail.com>
2015-10-09 10:55:36 +0100

    Commented out mysql

--- Website/app.js
+++ Website/app.js
@@ -1,20 +1,23 @@
 
 
 var express = require('express'),
-  config = require('./config/config'),
-  db = require('./app/models');
+  config = require('./config/config')
+  /*db = require('./app/models');*/
 
 var app = express();
 
 require('./config/express')(app, config);
-
+/*
 db.sequelize
   .sync()
   .then(function () {
+   */ 
     app.listen(config.port, function () {
       console.log('Express server listening on port ' + config.port);
     });
+  /*  
   }).catch(function (e) {
     throw new Error(e);
   });
+  */
 
